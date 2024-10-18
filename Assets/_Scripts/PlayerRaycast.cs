2ece55c854d9e9bf78388a5fb8e53c213ca705f4
Einar <einarsterten@gmail.com>
2024-10-18 16:55:07 +0200

    Player Raycast Script

---
+++ Assets/_Scripts/PlayerRaycast.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Burst.CompilerServices;
+using UnityEngine;
+
+public class PlayerRaycast : MonoBehaviour
+{
+    public LayerMask layersToHit;
+    public Transform player;
+    public Transform sun;
+
+    private void Update()
+    {
+        RaycastHit rayHit = new RaycastHit();
+        Ray ray = new Ray(sun.transform.position, player.position - transform.position);
+    }
+
+    /*void CheckForColliders()
+    {
+        if (Physics.Raycast(startPoint, endPoint))
+        {
+            Debug.Log("You are burning");
+        }
+
+        if (Physics.Raycast(theRay, out RaycastHit hit, range, layersToHit))
+        {
+            if (hit.collider.tag == "object")
+            {
+                Debug.Log("It's in the shadows");
+            }
+            else if (hit.collider.tag == "player")
+            {
+                Debug.Log("It's burning!");
+            }
+        }
+    }*/
+}
